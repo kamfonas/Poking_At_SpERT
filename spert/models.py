c23804d0fae52086214c829893fcdf9215a6752b
kamfonas <mkamfonas@infokarta.com>
2022-03-16 17:40:40 -0400

    adding longformer

--- spert/models.py
+++ spert/models.py
@@ -12,7 +12,7 @@ def get_token(h: torch.tensor, x: torch.tensor, token: int):
     """ Get specific token embedding (e.g. [CLS]) """
     emb_size = h.shape[-1]
 
-    token_h = h.view(-1, emb_size)
+    token_h = h.contiguous().view(-1, emb_size)
     flat = x.contiguous().view(-1)
 
     # get contextualized embedding of given token
